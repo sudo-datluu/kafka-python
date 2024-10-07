e2f021e863d2a1128ef89d206cf433fe16cef74b
datluu <tad.louis2810@gmail.com>
2024-10-07 15:14:42 +1100

    handle error code for api key

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 import socket  # noqa: F401
-
-def create_nessage(param_id: int) -> bytes:
-    # Convert the integer to a 4-byte big-endian byte array
-    id_bytes = param_id.to_bytes(4, byteorder='big')
-    header = len(id_bytes).to_bytes(4, byteorder='big')
-    return  header + id_bytes
+from .utils import create_message
+from .api import Header
 
 def handle_client(client: socket.socket):
-    req = client.recv(1024) # read data from client
-    correlation_id = int.from_bytes(req[8:12], byteorder='big')
-    client.sendall(create_nessage(correlation_id)) # send data to client
+    req = client.recv(1024) # read first 1024 bytes of data from client
+    header = Header(req)
+    print(header)
+    error = None if header.isAllowed() else 35
+    client.sendall(create_message(param_id=header.correlation_id, error=error)) # send data to client
     client.close()
 
 def main():
@@ -19,7 +17,6 @@ def main():
     while True:
         client, _ = server.accept() # wait for client
         handle_client(client)
-        
 
 if __name__ == "__main__":
     main()
